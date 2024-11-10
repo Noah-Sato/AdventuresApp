8a1bd093068c7154a482c68aabf4615fde2f0aef
Noah Elsner <main@Noahs-MacBook-Pro.local>
2024-11-10 23:44:01 +0000

    streamChat provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ import { Chat } from 'stream-chat-expo';
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useEffect } from 'react';
+import ChatProvider from '~/contexts/ChatProvider';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -18,48 +19,21 @@ export const unstable_settings = {
 
 export default function RootLayout() {
 
-
-
-  const client = StreamChat.getInstance('4wdb6xkueecd');
-
-  useEffect(()=>{
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: 'jDoe',
-          name: 'John Doe',
-          image: ''
-        },
-        client.devToken('jDoe'),
-      );
-      
-      const channel = client.channel('messaging', 'the_park', {
-        name: 'The Park',
-      });
-      await channel.create();
-    }
-
-
-    connect();
-  },[])
-
   return (
     
     <AuthProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <OverlayProvider>
-          <Chat client={client}>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="channel"  />
-              <Stack.Screen name="event/[id]" options={{ headerShown: false }} />
-              <Stack.Screen name="modal" options={{ presentation: 'modal', headerShown: false  }} />
-              <Stack.Screen name="editUserModal" options={{ presentation: 'modal', headerShown: false  }} />
-              <Stack.Screen name="guestListModal" options={{ presentation: 'modal', headerShown: false  }} />
-            </Stack>
-          </Chat>
-        </OverlayProvider>
+        <ChatProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="channel"  />
+            <Stack.Screen name="event/[id]" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal', headerShown: false  }} />
+            <Stack.Screen name="editUserModal" options={{ presentation: 'modal', headerShown: false  }} />
+            <Stack.Screen name="guestListModal" options={{ presentation: 'modal', headerShown: false  }} />
+          </Stack>
+        </ChatProvider>
       </GestureHandlerRootView>
     </AuthProvider>
   );
---
+++ contexts/ChatProvider.tsx
@@ -0,0 +1,57 @@
+import { PropsWithChildren, useState, useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+import { StreamChat } from 'stream-chat';
+import { Chat, OverlayProvider } from 'stream-chat-expo';
+
+
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
+
+
+export default function ChatProvider({children}: PropsWithChildren) {
+
+    const [isReady, setIsReady] = useState(false)
+
+    useEffect(()=>{
+        const connect = async () => {
+          await client.connectUser(
+            {
+              id: 'jDoe',
+              name: 'John Doe',
+              image: ''
+            },
+            client.devToken('jDoe'),
+          );
+          setIsReady(true)
+          
+          /*const channel = client.channel('messaging', 'the_park', {
+            name: 'The Park',
+          });
+          await channel.create();*/
+        }
+        connect();
+        return() => {
+            client.disconnectUser();
+            setIsReady(false);
+        };
+      },[])
+
+
+      if (!isReady) {
+        return(
+            <View style={{flex:1, alignItems:'center'}}>
+             <ActivityIndicator/>
+             </View>
+            )
+      }
+
+    return(
+        <OverlayProvider>
+            <Chat client={client}>
+                {children}
+            </Chat>
+        </OverlayProvider>
+
+
+    )
+}
